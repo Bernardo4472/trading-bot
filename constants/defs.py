11533e1e3910c776a1071607da31da358d8d3e31
Rokas Kaziulis <82209211+Bernardo4472@users.noreply.github.com>
2023-12-27 16:00:54 +0200

    Add files via upload

---
+++ constants/defs.py
@@ -0,0 +1,7 @@
+API_KEY = "af4ad6edf6b364c984b0403ecf9cd10c-ba4703210317d4ecbec6142ac96af6f0"
+ACCOUNT_ID = "101-012-22482455-001"
+OANDA_URL = "https://api-fxpractice.oanda.com/v3"
+
+SELL = -1
+BUY = 1
+NONE = 0
